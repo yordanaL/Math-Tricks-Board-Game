b79c289cdd9c2b048b91cf0c2d7b78c75d34225c
yordanaL <yblucheva@gmail.com>
2024-12-31 17:13:44 +0200

    Add input board size validation function
    The input board size validation function checks whether the input board length is minimum 4 and the input width is also minimum 4.

--- Math Tricks/Math Tricks.cpp
+++ Math Tricks/Math Tricks.cpp
@@ -3,11 +3,23 @@ using namespace std;
 
 const size_t MIN_BOARD_LENGTH = 4, MIN_BOARD_WIDTH = 4;
 
+bool isInputBoardSizeValid(size_t boardLength, size_t boardWidth);
+
 int main() {
+    //read the board size from console
     size_t boardLength = MIN_BOARD_LENGTH, boardWidth = MIN_BOARD_WIDTH;
-    cout << "To start the game enter the board size.";
+    cout << "To start the game enter the board size." << endl;
     cout << "Enter the board length: ";
     cin >> boardLength;
     cout << "Enter the board width: ";
     cin >> boardWidth;
+
+    return 0;
+}
+
+bool isInputBoardSizeValid(size_t boardLength, size_t boardWidth) {
+    bool isInputBoardLengthValid = boardLength >= MIN_BOARD_LENGTH;
+    bool isInputBoardWidthValid = boardWidth >= MIN_BOARD_WIDTH;
+
+    return (isInputBoardLengthValid && isInputBoardWidthValid);
 }
