603f9edc25b2d10ceda0ddf72efd5221be0254a3
yordanaL <yblucheva@gmail.com>
2024-12-31 17:40:00 +0200

    Added function to clear the console
    This function will help to achieve a clearer interface.

--- Math Tricks/Math Tricks.cpp
+++ Math Tricks/Math Tricks.cpp
@@ -4,22 +4,32 @@ using namespace std;
 const size_t MIN_BOARD_LENGTH = 4, MIN_BOARD_WIDTH = 4;
 
 bool isInputBoardSizeValid(size_t boardLength, size_t boardWidth);
+void clearConsole();
 
 int main() {
     //read the board size from console
     size_t boardLength = MIN_BOARD_LENGTH, boardWidth = MIN_BOARD_WIDTH;
     cout << "To start the game enter the board size." << endl;
-    cout << "Enter the board length: ";
+    cout << "Enter the board length (>= 4): ";
     cin >> boardLength;
-    cout << "Enter the board width: ";
+    cout << "Enter the board width (>= 4): ";
     cin >> boardWidth;
+    cout << endl;
+
+    clearConsole();
 
     return 0;
 }
 
+//function to check whether the board length and width are minimum 4
 bool isInputBoardSizeValid(size_t boardLength, size_t boardWidth) {
     bool isInputBoardLengthValid = boardLength >= MIN_BOARD_LENGTH;
     bool isInputBoardWidthValid = boardWidth >= MIN_BOARD_WIDTH;
 
     return (isInputBoardLengthValid && isInputBoardWidthValid);
+}
+
+//function to clear the console for clearer interface
+void clearConsole() {
+    system("cls");
 }
