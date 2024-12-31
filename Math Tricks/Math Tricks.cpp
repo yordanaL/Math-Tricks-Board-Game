4263c7ceffc52d838dabb227209feecaaf57ec4c
yordanaL <yblucheva@gmail.com>
2024-12-31 16:59:58 +0200

    Read board size from console

--- Math Tricks/Math Tricks.cpp
+++ Math Tricks/Math Tricks.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+const size_t MIN_BOARD_LENGTH = 4, MIN_BOARD_WIDTH = 4;
+
 int main() {
-    
+    size_t boardLength = MIN_BOARD_LENGTH, boardWidth = MIN_BOARD_WIDTH;
+    cout << "To start the game enter the board size.";
+    cout << "Enter the board length: ";
+    cin >> boardLength;
+    cout << "Enter the board width: ";
+    cin >> boardWidth;
 }
